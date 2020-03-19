cdc3d82dc76b2f81adf68fcea35ae9af618c6cd3
p2445061 <58169043+p2445061@users.noreply.github.com>
2020-03-19 14:21:39 +0000

    Delete clsOrder.cs

--- HardwareClasses/clsOrder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hardware_Testing
-{
-    internal class clsOrder
-    {
-        public int CustomerID { get; internal set; }
-        public int StaffID { get; internal set; }
-        public int OrderID { get; internal set; }
-    }
-}
