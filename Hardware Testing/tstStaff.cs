71df90f4718fe45a833b1f5fa5b5ef8860b349cc
p2445061 <58169043+p2445061@users.noreply.github.com>
2020-03-19 14:20:38 +0000

    Add files via upload

---
+++ Hardware Testing/tstStaff.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using HardwareClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hardware_Testing
+{
+    [TestClass]
+    public class tstStaff
+    {
+        string ID = "1";
+        string Name = "James";
+        string Address = "40 Glenfield Road";
+        string DOB = "" + DateTime.Now.Date;
+        string Manager = "" + false;
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff staff = new clsStaff();
+
+            Assert.IsNotNull(staff);
+        }
+
+        [TestMethod]
+        public void ManagerPropertyOK()
+        {
+            clsStaff staff = new clsStaff();
+            Boolean TestData = true;
+            staff.Manager = TestData;
+            Assert.AreEqual(staff.Manager, TestData);
+        }
+
+        [TestMethod]
+        public void DOBPropertyOK()
+        {
+            clsStaff staff = new clsStaff();
+            DateTime TestData = DateTime.Now.Date;
+            staff.DOB = TestData;
+            Assert.AreEqual(staff.DOB, TestData);
+        }
+
+        [TestMethod]
+        public void AddressPropertyOK()
+        {
+            clsStaff staff = new clsStaff();
+            string TestData = "";
+            staff.Address = TestData;
+            Assert.AreEqual(staff.Address, TestData);
+        }
+
+        [TestMethod]
+        public void NamePropertyOK()
+        {
+            clsStaff staff = new clsStaff();
+            string TestData = "";
+            staff.Name = TestData;
+            Assert.AreEqual(staff.Name, TestData);
+        }
+
+        [TestMethod]
+        public void IDPropertyOK()
+        {
+            clsStaff staff = new clsStaff();
+            int TestData = 1;
+            staff.ID = TestData;
+            Assert.AreEqual(staff.ID, TestData);
+        }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            clsStaff staff = new clsStaff();
+            Boolean Found = false;
+            int id = 1;
+            staff.ID = id;
+            Found = staff.Find(id);
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestNameFound()
+        {
+            clsStaff staff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            int id = 1;
+            staff.ID = id;
+            Found = staff.Find(id);
+            if (staff.Name != "James Smith")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestDOBFound()
+        {
+            clsStaff staff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            int id = 1;
+            staff.ID = id;
+            Found = staff.Find(id);
+            if (staff.DOB != Convert.ToDateTime("4/12/1999"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestAddressFound()
+        {
+            clsStaff staff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            int id = 1;
+            staff.ID = id;
+            Found = staff.Find(id);
+            if (staff.Address != "40 Glenfield Road")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestIDFound()
+        {
+            clsStaff staff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            int id = 1;
+            staff.ID = id;
+            Found = staff.Find(id);
+            if (staff.ID != 1)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestManagerFound()
+        {
+            clsStaff staff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            int id = 1;
+            staff.ID = id;
+            Found = staff.Find(id);
+            if (staff.Manager != false)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        //name
+
+        [TestMethod]
+        public void NameMinLessOne()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Name = "";
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void NameMin()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Name = "a";
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void NameMinPlusOne()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Name = "aa";
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void NameMaxLessOne()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Name = "";
+            Name = Name.PadRight(49,'a');
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void NameMax()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Name = "";
+            Name = Name.PadRight(50, 'a');
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void NameMaxPlusOne()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Name = "";
+            Name = Name.PadRight(51, 'a');
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void NameMid()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Name = "";
+            Name = Name.PadRight(25, 'a');
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void NameEXMax()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Name = "";
+            Name = Name.PadRight(500, 'a');
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        //address
+
+        [TestMethod]
+        public void AddressMinLessOne()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Address = "";
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMin()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Address = "a";
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMinPlusOne()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Address = "aa";
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMaxLessOne()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Address = "";
+            Address = Address.PadRight(49, 'a');
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMax()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Address = "";
+            Address = Address.PadRight(50, 'a');
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMaxPlusOne()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Address = "";
+            Address = Address.PadRight(51, 'a');
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMid()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Address = "";
+            Address = Address.PadRight(25, 'a');
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressEXMax()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            Address = "";
+            Address = Address.PadRight(500, 'a');
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        //dob
+
+        [TestMethod]
+        public void DOBEXMin()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(-100);
+            DOB = TestDate.ToString();
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DOBMin()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddDays(-1);
+            DOB = TestDate.ToString();
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DOBMax()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            DOB = TestDate.ToString();
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DOBMaxPlusOne()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddDays(1);
+            DOB = TestDate.ToString();
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DOBEXMax()
+        {
+            clsStaff staff = new clsStaff();
+            string Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(100);
+            DOB = TestDate.ToString();
+            Error = staff.Valid(ID, Name, Address, DOB, Manager);
+            Assert.AreNotEqual(Error, "");
+        }
+    }
+}
---
+++ Hardware Testing/tstStaffCollection.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using HardwareClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Hardware_Testing
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            Assert.IsNotNull(AllStaff);
+        }
+
+        [TestMethod]
+        public void StaffListOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            List<clsStaff> TestList = new List<clsStaff>();
+            clsStaff TestItem = new clsStaff();
+            TestItem.ID = 1;
+            TestItem.Name = "James";
+            TestItem.Address = "40 Glenfield Road";
+            TestItem.DOB = DateTime.Now.Date;
+            TestItem.Manager = false;
+            TestList.Add(TestItem);
+            AllStaff.StaffList = TestList;
+            Assert.AreEqual(AllStaff.StaffList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisStaffOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            clsStaff TestStaff = new clsStaff();
+            TestStaff.ID = 1;
+            TestStaff.Name = "James";
+            TestStaff.Address = "40 Glenfield Road";
+            TestStaff.DOB = DateTime.Now.Date;
+            TestStaff.Manager = false;
+            AllStaff.ThisStaff = TestStaff;
+            Assert.AreEqual(AllStaff.ThisStaff, TestStaff);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            List<clsStaff> TestList = new List<clsStaff>();
+            clsStaff TestItem = new clsStaff();
+            TestItem.ID = 1;
+            TestItem.Name = "James";
+            TestItem.Address = "40 Glenfield Road";
+            TestItem.DOB = DateTime.Now.Date;
+            TestItem.Manager = false;
+            TestList.Add(TestItem);
+            AllStaff.StaffList = TestList;
+            Assert.AreEqual(AllStaff.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            clsStaff TestStaff = new clsStaff();
+            int PrimaryKey = 1;
+            TestStaff.ID = 1;
+            TestStaff.Name = "James";
+            TestStaff.Address = "40 Glenfield Road";
+            TestStaff.DOB = DateTime.Now.Date;
+            TestStaff.Manager = false;
+            AllStaff.ThisStaff = TestStaff;
+            PrimaryKey = AllStaff.Add();
+            TestStaff.ID = PrimaryKey;
+            AllStaff.ThisStaff.Find(PrimaryKey);
+            Assert.AreEqual(AllStaff.ThisStaff, TestStaff);
+        }
+
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            clsStaff TestStaff = new clsStaff();
+            int PrimaryKey = 0;
+            TestStaff.ID = 1;
+            TestStaff.Name = "James";
+            TestStaff.Address = "40 Glenfield Road";
+            TestStaff.DOB = DateTime.Now.Date;
+            TestStaff.Manager = false;
+            AllStaff.ThisStaff = TestStaff;
+            PrimaryKey = AllStaff.Add();
+            TestStaff.ID = PrimaryKey;
+            AllStaff.ThisStaff.Find(PrimaryKey);
+            AllStaff.Delete();
+            Boolean Found = AllStaff.ThisStaff.Find(PrimaryKey);
+            Assert.IsFalse(Found);
+        }
+
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            clsStaff TestStaff = new clsStaff();
+            int PrimaryKey = 0;
+            TestStaff.ID = 1;
+            TestStaff.Name = "James";
+            TestStaff.Address = "40 Glenfield Road";
+            TestStaff.DOB = DateTime.Now.Date;
+            TestStaff.Manager = false;
+            AllStaff.ThisStaff = TestStaff;
+            PrimaryKey = AllStaff.Add();
+            TestStaff.ID = PrimaryKey;
+            TestStaff.ID = 2;
+            TestStaff.Name = "Troy";
+            TestStaff.Address = "24 Mill Lane";
+            TestStaff.DOB = DateTime.Now.Date;
+            TestStaff.Manager = true;
+            AllStaff.ThisStaff = TestStaff;
+            AllStaff.Update();
+            AllStaff.ThisStaff.Find(PrimaryKey);
+            Assert.AreEqual(AllStaff.ThisStaff, TestStaff);
+        }
+
+        [TestMethod]
+        public void ReportByNameMethodOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            clsStaffCollection FilteredStaff = new clsStaffCollection();
+            FilteredStaff.ReportByName("");
+            Assert.AreEqual(AllStaff.Count,FilteredStaff.Count);
+        }
+
+        [TestMethod]
+        public void ReportByNameTestDataFoundOK()
+        {
+            clsStaffCollection FilteredStaff = new clsStaffCollection();
+            Boolean OK = true;
+            FilteredStaff.ReportByName("James Smith");
+            if (FilteredStaff.Count == 1)
+            {
+                if (FilteredStaff.StaffList[0].ID != 1)
+                {
+                    OK = false;
+                }
+            }
+            else
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+    }
+}
